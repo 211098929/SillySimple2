d9fe92830d975ab1dd48b16898adcbbb8e8daabf
Siboniso <smsimango29@gmail.com>
2014-08-21 15:49:32 +0200

    done

--- SillySimple2/app.js
+++ SillySimple2/app.js
@@ -1,5 +1,7 @@
-$(document).on('pageinit','#splash',function(){ // the .on() method does require jQuery 1.7 + but this will allow you to have the contained code only run when the #splash page is initialized.
-    setTimeout(function(){
-        $.mobile.changePage("index.html", "fade");
-    }, 4000);
+$(function() {
+  setTimeout(hideSplash, 2000);
 });
+
+function hideSplash() {
+  $.mobile.changePage("#ui-content", "fade");
+}
