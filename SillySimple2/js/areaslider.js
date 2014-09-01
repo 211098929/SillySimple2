b2748be90de8bd5c4fd3678c8db4137167fffe6a
Siboniso <smsimango29@gmail.com>
2014-09-01 10:33:15 +0200

    done

--- SillySimple2/js/areaslider.js
+++ SillySimple2/js/areaslider.js
@@ -1,48 +1,39 @@
 sliderInt=1;
-sliderNext=2;
+roomId = 1;
 count = $("#main_area >img").size();
 $(document).ready(function(){
-$("#main_area > img#1").fadeIn(300);
-	startSlider();
+	$("#main_area > img#1").fadeIn(600);
 });
-function startSlider(){
-	count = $("#main_area >img").size();
-	loop = setInterval(function(){
-					
-	if(sliderNext > count)
-	{
-		sliderNext=1;
-		sliderInt=1;
-	}
-					
-	$("#main_area >img").fadeOut(300);
-	$("#main_area >img#" + sliderNext).fadeIn(300);
-	sliderInt = sliderNext;
-	sliderNext = sliderNext + 1;
-	},3000)
-	}
-function stopLoop(){
-	window.clearInterval(loop);
-}
-function prev(){
-	newSlide = sliderInt - 1;
-	showSlide(newSlide);
-	}
 function next(){
-	newSlide = sliderInt + 1;
-	showSlide(newSlide);
-	}
-function showSlide(id){
-	stopLoop();
-	if(id > count){
-		id = 1;
-	}
-	else if(id < 1){
-		id = count;
+		count = $("#main_area >img").size();
+		sliderInt += 1;
+		if(sliderInt <= count){
+			$("#main_area >img").fadeOut(600);
+			$("#main_area >img#" + sliderInt).fadeIn(600);
+			setRoomID(sliderInt);
 		}
-	$("#main_area >img").fadeOut(300);
-	$("#main_area >img#" + sliderNext).fadeIn(300);
-	sliderInt = id;
-	sliderNext = id + 1;
+		else
+		{
+			alert("No more images");
+		}
 	}
-	
+function prev(){
+		sliderInt  = sliderInt - 1;
+		if(sliderInt > 0)
+		{
+			$("#main_area >img").fadeOut(600);
+			$("#main_area >img#" + sliderInt).fadeIn(600);
+			setRoomID(sliderInt);
+		}
+		else{
+			alert("No more images");
+		}
+	}
+function setRoomID(id)
+{
+	roomId = id;
+}
+function getRoomID()
+{
+	return roomId;
+}
